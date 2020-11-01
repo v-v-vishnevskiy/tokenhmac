9dc5160d105c61ae06cfa4e2e5f7959e79c4c61d
Валерий Вишневский <v.vishnevskiy@v-vishnevskiy.local>
2020-11-01 19:46:47 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 with open(os.path.join(os.path.dirname(__file__), "tokenhmac", "__init__.py")) as f:
     for line in f:
@@ -12,12 +12,23 @@ with open(os.path.join(os.path.dirname(__file__), "tokenhmac", "__init__.py")) a
         raise RuntimeError("Unable to read the version from tokenhmac/__init__.py")
 
 
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as f:
+    readme = f.read()
+
+
 setup(
     name="tokenhmac",
     version=VERSION,
-    author="Valeriy Vishnevskiy",
+    author="Valery Vishnevskiy",
     author_email="v.v.vishnevskiy@yandex.ru",
-    license="as-is",
-    packages=find_packages(),
+    url="https://github.com/v-v-vishnevskiy/tokenhmac",
+    project_urls={"GitHub: repo": "https://github.com/v-v-vishnevskiy/tokenhmac"},
+    description="Fast JWT",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     classifiers=["Operating System :: Unix", "Programming Language :: Python", "Programming Language :: Python :: 3"],
+    license="MIT",
+    keywords=["jwt", "token", "HMAC", "HS256", "HS384", "HS512", "encryption"],
+    packages=["tokenhmac"],
+    provides=["tokenhmac"],
 )
